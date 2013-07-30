92a4e9777d07df723ec4967b55b4872b40014b01
Clark Chen <clark@kleric.org>
2013-07-30 17:49:22 +0800

    Fixed spacing
    It was weird

--- src/org/kleric/happyfuntime/Shamir.java
+++ src/org/kleric/happyfuntime/Shamir.java
@@ -17,15 +17,15 @@ public class Shamir {
 	public static final String URL = "https://docs.google.com/feeds/download/spreadsheets/Export?key=0AnN-5p9SwIfUdHl4Rl9lRGg5VjRoR1pISVdMLWk5TGc&amp;exportFormat=csv&amp;gid=0";
 	public static final String SPREADSHEET_NAME = "Shamir's Key, 15-151";
 	// The minimum number of keys 
-    private static final int requiredNumber = 50; 
+	private static final int requiredNumber = 50; 
 	private static final BigInteger prime = new BigInteger("5992830235524142758386850633773258681119");
 
 	/** 
-     * @author Rahul 
-     * @param shares 
-     *            Key-Value pairs of data points 
-     * @return the secret number 
-     */
+	 * @author Rahul 
+	 * @param shares 
+	 *            Key-Value pairs of data points 
+	 * @return the secret number 
+	 */
 	private static BigInteger join(HashMap<Integer, BigInteger> shares) {
 		BigInteger secret = new BigInteger("0");
 		for (int a : shares.keySet()) 
@@ -62,7 +62,7 @@ public class Shamir {
 		service.setUserCredentials(user, pass);
 		// Define the URL to request.  This should never change.
 		URL SPREADSHEET_FEED_URL = new URL(
-		        "https://spreadsheets.google.com/feeds/spreadsheets/private/full");
+				"https://spreadsheets.google.com/feeds/spreadsheets/private/full");
 		// Make a request to the API and get all spreadsheets.
 		SpreadsheetFeed feed = service.getFeed(SPREADSHEET_FEED_URL,
 				SpreadsheetFeed.class);
@@ -81,28 +81,28 @@ public class Shamir {
 				spreadsheet.getWorksheetFeedUrl(), WorksheetFeed.class);
 		List<WorksheetEntry> worksheets = worksheetFeed.getEntries();
 		WorksheetEntry worksheet = worksheets.get(0);
-		
-		// Fetch the cell feed of the worksheet.
-	    URL cellFeedUrl = worksheet.getCellFeedUrl();
-	    CellFeed cellFeed = service.getFeed(cellFeedUrl, CellFeed.class);
 
-	    Integer cellBTemp = 0;
-	    for (CellEntry cell : cellFeed.getEntries()) 
-	    {
-	    	if(cell.getTitle().getPlainText().contains("B") && !cell.getTitle().getPlainText().contains("B1"))
-	    	{
-	    		//This cell is part of Key (X)
-	    		cellBTemp = cell.getCell().getNumericValue().intValue();
-	    	}
-	    	else if(cell.getTitle().getPlainText().contains("C") && !cell.getTitle().getPlainText().contains("C1"))
-	    	{
-	    		map.put(cellBTemp, new BigInteger(cell.getCell().getInputValue()));
-	    		System.out.println(cellBTemp + ", " + cell.getCell().getInputValue());
-	    	}
-	    }
-	    return map;
+		// Fetch the cell feed of the worksheet.
+		URL cellFeedUrl = worksheet.getCellFeedUrl();
+		CellFeed cellFeed = service.getFeed(cellFeedUrl, CellFeed.class);
+
+		Integer cellBTemp = 0;
+		for (CellEntry cell : cellFeed.getEntries()) 
+		{
+			if(cell.getTitle().getPlainText().contains("B") && !cell.getTitle().getPlainText().contains("B1"))
+			{
+				//This cell is part of Key (X)
+				cellBTemp = cell.getCell().getNumericValue().intValue();
+			}
+			else if(cell.getTitle().getPlainText().contains("C") && !cell.getTitle().getPlainText().contains("C1"))
+			{
+				map.put(cellBTemp, new BigInteger(cell.getCell().getInputValue()));
+				System.out.println(cellBTemp + ", " + cell.getCell().getInputValue());
+			}
+		}
+		return map;
 	}
-	
+
 	public static void main(String[] args) throws IOException, ServiceException, URISyntaxException
 	{
 		String USERNAME = "throwaway7826@gmail.com";
@@ -113,35 +113,35 @@ public class Shamir {
 			PASSWORD = args[1];
 		}
 
-	    HashMap<Integer, BigInteger> map = getData(USERNAME, PASSWORD);
-	    if(map == null)
-	    {
-	    	System.err.println("Unable to retrieve the data.");
-	    }
-	    else if(map.keySet().size() >= requiredNumber)
-	    {
-	    	System.out.println("Proper Number of Keys Available! Beginning Calculation.");
-	    	String value = join(map).toString(); 
-	        System.out.println(value); 
-	        printString(value);
-	    }
-	    else
-	    {
-	    	System.out.println("Not enough keys available...");
-	    	System.out.println("Still need " + (requiredNumber - map.keySet().size()) + " keys");
-	    }
+		HashMap<Integer, BigInteger> map = getData(USERNAME, PASSWORD);
+		if(map == null)
+		{
+			System.err.println("Unable to retrieve the data.");
+		}
+		else if(map.keySet().size() >= requiredNumber)
+		{
+			System.out.println("Proper Number of Keys Available! Beginning Calculation.");
+			String value = join(map).toString(); 
+			System.out.println(value); 
+			printString(value);
+		}
+		else
+		{
+			System.out.println("Not enough keys available...");
+			System.out.println("Still need " + (requiredNumber - map.keySet().size()) + " keys");
+		}
 	}
 	/** 
-     * @author Rahul 
-     * @param number 
-     *            The BigInteger secret number 
-     * @return the string answer to the question 
-     */
-    public static String printString(String number) { 
-        String string = ""; 
-        for (int a = 0; a < number.length(); a += 2) 
-            string += Character.toChars(54 + Integer.parseInt(number.substring( 
-                    a, a + 2))/* % 26 + 11//Enable this if you need to */); 
-        return string; 
-    } 
+	 * @author Rahul 
+	 * @param number 
+	 *            The BigInteger secret number 
+	 * @return the string answer to the question 
+	 */
+	public static String printString(String number) { 
+		String string = ""; 
+		for (int a = 0; a < number.length(); a += 2) 
+			string += Character.toChars(54 + Integer.parseInt(number.substring( 
+					a, a + 2))/* % 26 + 11//Enable this if you need to */); 
+		return string; 
+	} 
 }
